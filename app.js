f006056c280b5447c35b245a74d13892707814f4
Collin Schaafsma <collin@quickleft.com>
2012-02-19 23:28:16 -0700

    auto adds folks as they connect, multi video

--- app.js
+++ app.js
@@ -34,13 +34,23 @@ app.configure('production', function(){
 app.get( '/', routes.index );
 
 // Socket.io
-io.sockets.on('connection', function(client) {
-  client.on('capture', function(data) {
-    io.sockets.emit('capture', data);
+io.sockets.on('connection', function(socket) {
+  var clients = io.sockets.clients();
+  var client_ids = [];
+  for (var i = 0; i < clients.length; ++i) {
+    client_ids[i] = clients[i].id;
+  }
+
+  socket.emit('list_clients', { clients: client_ids })
+  socket.broadcast.emit('join', { client_id: socket.id });
+  socket.on('capture', function(data) {
+    io.sockets.emit('capture', { stream_data: data, client_id: socket.id });
   });
-  client.on('ping', function(data) {
-    console.log(data);
+
+  socket.on('disconnect', function () {
+    io.sockets.emit('leave', { client_id: socket.id });
   });
+
 });
 
 app.listen( process.env.PORT || 3000 );
--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -80,32 +80,43 @@ namespace.init = function( $ ){
 
   var
     video        = document.getElementById('monitor'),
-    capture      = document.getElementById('capture'),
-    receive      = document.getElementById('receive');
+    capture      = document.getElementById('capture');
 
-    socket.emit('ping', { yo: 'talk to me' });
+  socket.on('list_clients', function(data) {
+    for (var i = 0; i < data.clients.length; ++i) {
+      var receive = $('<li id="receive_'+ data.clients[i] +'"><img width="160" height="120"/></li>');
+      $('#online').append(receive);
+    }
+  });
 
-    // Eat it
-    socket.on('capture', function(data) {
-      receive.src = data;
-      console.log("eat it");
+  socket.on('join', function(data) {
+    var receive = $('<li id="receive_'+ data.client_id +'"><img width="160" height="120"/></li>');
+    $('#online').append(receive);
+  });
+
+  socket.on('leave', function(data) {
+    $('#receive_'+data.client_id).remove();
+  });
+
+  // Eat it
+  socket.on('capture', function(data) {
+    $('#receive_'+data.client_id+' img').attr('src', data.stream_data);
+  });
+
+  // Pull it
+  navigator.webkitGetUserMedia('video audio', function(stream) {
+    video.src = webkitURL.createObjectURL(stream);
+    video.addEventListener('error', function () {
+      stream.stop();
     });
+  });
 
-    // Pull it
-    navigator.webkitGetUserMedia('video audio', function(stream) {
-      video.src = webkitURL.createObjectURL(stream);
-      video.addEventListener('error', function () {
-        stream.stop();
-      });
-    });
-
-    // Push it
-    $(video).on('timeupdate', function(e) {
-      var captureCanvas = capture.getContext('2d');
-      captureCanvas.drawImage(video, 80, 0, 160, 120, 0, 0, 160, 120);
-      socket.emit('capture', capture.toDataURL());
-      console.log("push it");
-    });
+  // Push it
+  $(video).on('timeupdate', function(e) {
+    var captureCanvas = capture.getContext('2d');
+    captureCanvas.drawImage(video, 80, 0, 160, 120, 0, 0, 160, 120);
+    socket.emit('capture', capture.toDataURL());
+  });
 
 };
 
